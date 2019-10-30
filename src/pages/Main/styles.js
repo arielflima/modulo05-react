c52cd9643bf9e7e734f2efa6627a33a04d14d2d3
arielflima <arielflima@gmail.com>
2019-10-30 18:44:46 -0300

    usando styled-components global

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 54px;
-  color: ${props => (props.error ? 'red' : '#7159c1')};
-  font-family: Ariel, Helvetica, sans-serif;
-
-  small {
-    font-size: 14px;
-    color: #333;
-  }
+  color: #fff;
 `;
